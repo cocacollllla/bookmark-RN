8053e2ca00ddd3048d08490039bf866c5d4d1f21
cocacollllla <cocacollllla@gmail.com>
2022-11-15 19:05:13 +0900

    수정

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>Welcome</div>
+      <div onClick={() => navigate("/login")}>로그인페이지로</div>
+    </div>
+  );
 };
 
 export default Welcome;
